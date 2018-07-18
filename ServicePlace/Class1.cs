b60d138bd8de4a6f82480f47790b01e6439f9262
obejnen <obejnen@gmail.com>
2018-07-18 12:22:52 +0300

    Add project files.

---
+++ ServicePlace/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicePlace
+{
+    public class Class1
+    {
+    }
+}
