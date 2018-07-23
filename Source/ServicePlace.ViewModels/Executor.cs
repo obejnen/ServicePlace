86cd359cbad226bfc390da6789921544c33f39ff
obejnen <obejnen@gmail.com>
2018-07-23 22:03:19 +0300

    change model properties

--- Source/ServicePlace.ViewModels/Executor.cs
+++ Source/ServicePlace.ViewModels/Executor.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ServicePlace.ViewModels
@@ -12,5 +13,9 @@ namespace ServicePlace.ViewModels
         [StringLength(200)]
         public string Title { get; set; }
         public string Body { get; set; }
+        public decimal Price { get; set; }
+        public string UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
     }
 }
