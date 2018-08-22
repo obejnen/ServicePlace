135678cefda7794f15963e109bf1794fc988a66d
obejnen <obejnen@gmail.com>
2018-08-22 09:29:29 +0300

    ru\en localization

--- Source/ServicePlace.Website/Controllers/HomeController.cs
+++ Source/ServicePlace.Website/Controllers/HomeController.cs
@@ -1,13 +1,42 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
 
 namespace ServicePlace.Website.Controllers
 {
     public class HomeController : Controller
     {
-
         public ActionResult Index()
         {
             return RedirectToAction("Index", "Order");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangeLang(string lang)
+        {
+            var returnUrl = Request.UrlReferrer?.AbsolutePath;
+            var cultures = new List<string>() { "ru", "en" };
+            if (!cultures.Contains(lang))
+            {
+                lang = "en";
+            }
+            var cookie = Request.Cookies["lang"];
+            if (cookie != null)
+                cookie.Value = lang;
+            else
+            {
+
+                cookie = new HttpCookie("lang")
+                {
+                    HttpOnly = false,
+                    Value = lang,
+                    Expires = DateTime.Now.AddYears(1)
+                };
+            }
+            Response.Cookies.Add(cookie);
+            return Redirect(returnUrl);
         }
     }
 }
